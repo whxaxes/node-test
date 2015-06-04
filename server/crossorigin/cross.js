74eea57bb8429b307dd4ad7304b36b87f8e672ca
王鸿兴 <851094531@qq.com>
2015-06-04 19:25:24 +0800

    update cross

--- server/crossorigin/cross.js
+++ server/crossorigin/cross.js
@@ -4,38 +4,40 @@ var router = require('easy-router');
 var querystring = require('querystring');
 
 router.setMap({
-    '/cross/jsonp' : jsonp,
-    '/cross/origin' : origin
+    '/cross/jsonp': jsonp,
+    '/cross/origin': origin
 });
 
-function jsonp(req , res){
+function jsonp(req, res) {
     var params = req.params;
 
     var data = [];
-    for(var k in params){
-        if(k !== "callback"){
-            data.push(k+":"+params[k]);
+    for (var k in params) {
+        if (k !== "callback") {
+            data.push(k + ":" + params[k]);
         }
     }
 
-    res.writeHead(200 , {'Content-Type':'application/javascript'});
-    res.end((params.callback||"console.log") + "('jsonp:"+data.join(",")+"')");
+    res.writeHead(200, {
+        'Content-Type': 'application/javascript'
+    });
+    res.end((params.callback || "console.log") + "('jsonp:" + data.join(",") + "')");
 }
 
-function origin(req , res){
+function origin(req, res) {
     var origin = req.headers['origin'];
 
-//    只允许本地url测试用
-    if(!origin || !/^http\:\/\/localhost(\:\d+)?/.test(origin)){
+    //    只允许本地url测试用
+    if (origin!=="null" && !/^http\:\/\/localhost(\:\d+)?/.test(origin)) {
         res.end();
     }
 
-    res.writeHead(200 , {
-        'Access-Control-Allow-Origin' : origin,
-        'Access-Control-Allow-Headers' : 'Access-Control-Allow-Origin'
+    res.writeHead(200, {
+        'Access-Control-Allow-Origin': origin,
+        'Access-Control-Allow-Headers': 'Access-Control-Allow-Origin'
     });
 
-    if(req.headers['Access-Control-Allow-Headers']){
+    if (req.headers['Access-Control-Allow-Headers']) {
         res.end();
         return;
     }
@@ -43,12 +45,11 @@ function origin(req , res){
     var data = "";
     req.setEncoding = 'utf-8';
 
-    req.on('data' , function(chunk){
+    req.on('data', function(chunk) {
         data += chunk;
     });
 
-    req.on('end' , function(){
+    req.on('end', function() {
         res.end(data);
     })
 }
-
