dd2b2cfddddc94751691787f5f6d80b8e73a7917
王鸿兴 <851094531@qq.com>
2015-08-19 14:17:57 +0800

    update

--- server/app.js
+++ server/app.js
@@ -12,9 +12,9 @@ global.PATH_LINE = __dirname.match(/\/|\\/)[0];
 var router = require('easy-router');
 
 router.setMap({
-    "/topic/*":STATIC_PATH+"txwork/topic_*.html",
+    "/topic/*": STATIC_PATH + "txwork/topic_*.html",
 
-    "/public/**/*":STATIC_PATH+"**/*"        //静态资源
+    "/public/**/*": STATIC_PATH + "**/*"        //静态资源
 });
 
 require('./bigpipe/bigpipe');
@@ -26,18 +26,20 @@ require('./crossorigin/cross');
 
 var websocket = require("./websocket/socket");
 
-var server = router.listen(9030);
+var server = http.createServer(function (req, res) {
+    router.route(req, res);
+}).listen(9030, "127.0.0.1");
 
-websocket.update(server , function(ws){
-    ws.on('close' , function(reason){
-        console.log("socket closed:"+reason);
+websocket.update(server, function (ws) {
+    ws.on('close', function (reason) {
+        console.log("socket closed:" + reason);
     });
 
-    ws.on('message' , function(data){
+    ws.on('message', function (data) {
         websocket.brocast(data);
     });
 });
 
-global.directTo404 = function(res){
+global.directTo404 = function (res) {
     router.error(res);
 };
