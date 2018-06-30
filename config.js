442609e872eeb2704a47972f2aeac87405e05794
wanghx <whxaxes@gmail.com>
2018-06-30 18:39:33 +0800

    feat: add pm2

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  apps: [
+    {
+      name: 'node-test',
+      script: './index.js',
+      env: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+};
--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
-var gulp = require("gulp");
-var livereload = require("gulp-livereload");
+var gulp = require('gulp');
+var livereload = require('gulp-livereload');
 
-gulp.task('watch' , function(){
-    livereload.listen();
+gulp.task('watch', function() {
+  livereload.listen();
 
-    gulp.watch(['**/*.css', '**/*.js', '**/*.html'], function(file){
-        livereload.changed(file.path)
-    });
+  gulp.watch(['**/*.css', '**/*.js', '**/*.html'], function(file) {
+    livereload.changed(file.path);
+  });
 });
